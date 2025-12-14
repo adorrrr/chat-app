14bf030e936df9ec89bce6351d60f1bbfc03b660
Ahasanul Monir Adar <2120476@iub.edu.bd>
2025-12-14 23:10:38 +0600

    push

--- index.js
+++ index.js
@@ -67,6 +67,14 @@ app.post("/chats", (req, res) => {
 
 
 
+//Edit route
+app.get("/chats/:id/edit", async (req, res) => {
+    let {id} = req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs", {chat});
+});
+
+
 
 
 
